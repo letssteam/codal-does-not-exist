9ec028b9bdacc176efb3af6ac3d4f4376b12b315
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-04-12 11:14:11 +0000

    Remove useless files

--- samples/Basic/ADC/ACDSample.cpp
+++
@@ -1,7 +0,0 @@
-#include "ADCSample.h"
-
-using namespace codal;
-
-void ADCSample_main(){
-    codal::STM32PNUCLEO_WB55RG PNUCLEO_WB55RG;
-}
--- samples/Basic/ADC/ADCSample.h
+++
@@ -1,9 +0,0 @@
-#include "STM32PNUCLEO_WB55RG.h"
-
-#ifndef ADC_SAMPLE_H
-#define ADC_SAMPLE_H
-
-#define SAMPLE_MAIN ADCSample_main
-void ADCSample_main();
-
-#endif
